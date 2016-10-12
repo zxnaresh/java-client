4d23c3f3800a9bf1cea035918dd99b547b529f96
zxnaresh <nareshbv2010@gmail.com>
2016-10-12 11:36:54 +0530

    Create SubscriptionHandler_broadcastMessage_specific.java

---
+++ signalr-client-sdk/src/main/java/microsoft/aspnet/signalr/client/hubs/SubscriptionHandler_broadcastMessage_specific.java
@@ -0,0 +1,5 @@
+package microsoft.aspnet.signalr.client.hubs;
+
+public interface SubscriptionHandler_broadcastMessage_specific<E1, E2, E3, E4, E5,E6,E7,E8> {
+    public void run(E1 p1, E2 p2, E3 p3, E4 p4, E5 p5,E6 p6,E7,p7,E8 p8);
+}
